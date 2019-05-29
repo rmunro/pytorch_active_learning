961b61b4f92d5a8c1a99894b8beec5e6c0cfd842
Robert Munro <robert.munro@gmail.com>
2019-05-28 23:21:11 -0700

    updated active learning script to match book content

--- active_learning_basics.py
+++ active_learning_basics.py
@@ -7,7 +7,7 @@ A simple text classification algorithm in PyTorch
 This is an open source example to accompany the book:
 "Human-in-the-Loop Machine Learning"
 
-This example is trys to classify news headlines into one of two categories:
+This example trys to classify news headlines into one of two categories:
   disaster-related
   not disaster-related
 
@@ -26,6 +26,8 @@ import csv
 import re
 import os
 from random import shuffle
+from collections import defaultdict	
+
 
 __author__ = "Robert Munro"
 __license__ = "MIT"
@@ -33,11 +35,11 @@ __version__ = "1.0.1"
 
 # settings
 
-minimum_evaluation_items = 600 # annotate this many randomly selected items first for evaluation data before creating training data
+minimum_evaluation_items = 1200 # annotate this many randomly sampled items first for evaluation data before creating training data
 minimum_training_items = 400 # minimum number of training items before we first train a model
 
-epochs = 20 # number of epochs per training session
-samples = 1000  # number to sample per epoch per label
+epochs = 10 # number of epochs per training session
+select_per_epoch = 200  # number to select per epoch per label
 
 
 data = []
@@ -60,12 +62,15 @@ already_labeled = {} # tracking what is already labeled
 feature_index = {} # feature mapping for one-hot encoding
 
 
-def load_data(filepath):
-    # csv format: ID, TEXT, LABEL (if any), SAMPLING_STRATEGY (if any) 
+def load_data(filepath, skip_already_labeled=False):
+    # csv format: [ID, TEXT, LABEL, SAMPLING_STRATEGY, CONFIDENCE]
     with open(filepath, 'r') as csvfile:
         data = []
         reader = csv.reader(csvfile)
         for row in reader:
+            if skip_already_labeled and row[0] in already_labeled:
+        	    continue
+        		
             if len(row) < 3:
                 row.append("") # add empty col for LABEL to add later
             if len(row) < 4:
@@ -96,21 +101,41 @@ def write_data(filepath, data):
 
 
 # LOAD ALL UNLABELED, TRAINING, VALIDATION, AND EVALUATION DATA
-data = load_data(unlabeled_data)
-
 training_data = load_data(training_related_data) + load_data(training_not_related_data)
 training_count = len(training_data)
     
 evaluation_data = load_data(evaluation_related_data) + load_data(evaluation_not_related_data)
 evaluation_count = len(evaluation_data)
 
+data = load_data(unlabeled_data, skip_already_labeled=True)
+
+annotation_instructions = "Please type 1 if this message is disaster-related, "
+annotation_instructions += "or hit Enter if not.\n"
+annotation_instructions += "Type 2 to go back to the last message, "
+annotation_instructions += "type d to see detailed definitions, "
+annotation_instructions += "or type s to save your annotations.\n"
+
+last_instruction = "All done!\n"
+last_instruction += "Type 2 to go back to change any labels,\n"
+last_instruction += "or Enter to save your annotations."
+
+detailed_instructions = "A 'disaster-related' headline is any story about a disaster.\n"
+detailed_instructions += "It includes:\n"
+detailed_instructions += "  - human, animal and plant disasters.\n"
+detailed_instructions += "  - the response to disasters (aid).\n"
+detailed_instructions += "  - natural disasters and man-made ones like wars.\n"
+detailed_instructions += "It does not include:\n"
+detailed_instructions += "  - criminal acts and non-disaster-related police work\n"
+detailed_instructions += "  - post-response activity like disaster-related memorials.\n\n"
+
 
 def get_annotations(data, default_sampling_strategy="random"):
-    """Prompts annotator for label from command line and add annotations to data 
+    """Prompts annotator for label from command line and adds annotations to data 
     
     Keyword arguments:
-        data -- an list of unlabeled items where each item is [ID, TEXT, LABEL, SAMPLING_STRATEGY, CONFIDENCE]
-        default_sampling_strategy -- the default sampling strategy to use for each item if not already specified
+        data -- an list of unlabeled items where each item is 
+                [ID, TEXT, LABEL, SAMPLING_STRATEGY, CONFIDENCE]
+        default_sampling_strategy -- strategy to use for each item if not already specified
     """
 
     ind = 0
@@ -127,30 +152,28 @@ def get_annotations(data, default_sampling_strategy="random"):
                 print("Skipping seen "+label)
                 ind+=1
             else:
-                print("Please type 1 if this message is disaster-related, or hit Enter if not.")
-                print("Type 2 to go back to the last message, or type s to save annotations\n")
-                label = str(input(text+"\n\n> "))  # NB: python 2.* will not accept an empty response but with otherwise work
+                print(annotation_instructions)
+                label = str(input(text+"\n\n> ")) 
 
-                if label == "2":
-                    # go back
-                    ind-=1
+                if label == "2":                   
+                    ind-=1  # go back
+                elif label == "d":                    
+                    print(detailed_instructions) # print detailed instructions
                 elif label == "s":
-                    # save and exit
-                    break
+                    break  # save and exit
                 else:
                     if not label == "1":
                         label = "0" # treat everything other than 1 as 0
-
+                        
                     data[ind][2] = label # add label to our data
 
                     if data[ind][3] is None or data[ind][3] == "":
-                        data[ind][3] = default_sampling_strategy # add default sampling strategy if none given
-
+                        data[ind][3] = default_sampling_strategy # add default if none given
                     ind+=1        
 
         else:
             #last one - give annotator a chance to go back
-            print("All done! Type 2 to go back to the last message if you need to change the label,\nor Enter to save your annotations.")
+            print(last_instruction)
             label = str(input("\n\n> ")) 
             if label == "2":
                 ind-=1
@@ -158,7 +181,6 @@ def get_annotations(data, default_sampling_strategy="random"):
                 ind+=1
 
     return data
-
 
 
 def create_features(minword = 3):
@@ -185,7 +207,10 @@ def create_features(minword = 3):
 
 
 class SimpleTextClassifier(nn.Module):  # inherit pytorch's nn.Module
+    """Text Classifier with 1 hidden layer 
 
+    """
+    
     def __init__(self, num_labels, vocab_size):
         super(SimpleTextClassifier, self).__init__() # call parent init
 
@@ -193,10 +218,10 @@ class SimpleTextClassifier(nn.Module):  # inherit pytorch's nn.Module
         self.linear1 = nn.Linear(vocab_size, 128)
         self.linear2 = nn.Linear(128, num_labels)
 
-    def forward(self, bow_vec):
+    def forward(self, feature_vec):
         # Define how data is passed through the model
 
-        hidden1 = self.linear1(bow_vec).clamp(min=0) # ReLU
+        hidden1 = self.linear1(feature_vec).clamp(min=0) # ReLU
         output = self.linear2(hidden1)
         return F.log_softmax(output, dim=1)
                                 
@@ -209,15 +234,17 @@ def make_feature_vector(features, feature_index):
     return vec.view(1, -1)
 
 
-def train_model(training_data, validation_data = "", evaluation_data = "", vocab_size=0):
+def train_model(training_data, validation_data = "", evaluation_data = "", num_labels=2, vocab_size=0):
     """Train model on the given training_data
 
     Tune with the validation_data
     Evaluate accuracy with the evaluation_data
     """
 
-    model = SimpleTextClassifier(2, vocab_size)
-    # let's hard-code our labels for this example code and map to meaningful booleans, so we don't mix them up
+    model = SimpleTextClassifier(num_labels, vocab_size)
+    # let's hard-code our labels for this example code 
+    # and map to the same meaningful booleans in our data, 
+    # so we don't mix anything up when inspecting our data
     label_to_ix = {"not_disaster_related": 0, "disaster_related": 1} 
 
     loss_function = nn.NLLLoss()
@@ -228,40 +255,34 @@ def train_model(training_data, validation_data = "", evaluation_data = "", vocab
         print("Epoch: "+str(epoch))
         current = 0
 
-        # current subset of data to use in this epoch
-        epoch_data = []
+        # make a subset of data to use in this epoch
+        # with an equal number of items from each label
 
-        not_related = []
-        related = []
-        shuffle(training_data) #randomize the order of the training data
-        for item in training_data:
-            label = item[2]
-            if label == "0" and len(not_related) < samples:
-                epoch_data.append(item)
-            elif label == "1" and len(related) < samples:
-                epoch_data.append(item)
-            elif len(not_related) >= samples and len(related) >= samples:
-                break        
-
+        shuffle(training_data) #randomize the order of the training data        
+        related = [row for row in training_data if '1' in row[2]]
+        not_related = [row for row in training_data if '0' in row[2]]
+        
+        epoch_data = related[:select_per_epoch]
+        epoch_data += not_related[:select_per_epoch]
+        shuffle(epoch_data) 
+                
+        # train our model
         for item in epoch_data:
             training_idx = random.randint(0,len(data)-1)
-            words = item[1].split()
+            features = item[1].split()
             label = int(item[2])
 
             model.zero_grad() 
 
-            feature_vec = make_feature_vector(words, feature_index)
+            feature_vec = make_feature_vector(features, feature_index)
             target = torch.LongTensor([int(label)])
-
-            # torch.LongTensor([label_to_ix[label]]) 
-
-            #make_target(label, label_to_ix)
 
             log_probs = model(feature_vec)
 
+			# compute loss function, do backward pass, and update the gradient
             loss = loss_function(log_probs, target)
             loss.backward()
-            optimizer.step()
+            optimizer.step()	
 
     fscore, auc = evaluate_model(model, evaluation_data)
     fscore = round(fscore,3)
@@ -278,11 +299,16 @@ def train_model(training_data, validation_data = "", evaluation_data = "", vocab
     return model_path
 
 
-def get_low_conf_unlabeled(model, unlabeled_data, number=80):
+def get_low_conf_unlabeled(model, unlabeled_data, number=80, limit=10000):
     confidences = []
-    print("Get confidences for unlabeled data (this might take a while)")
+    if limit == -1: # we're predicting confidence on *everything* this will take a while
+    	print("Get confidences for unlabeled data (this might take a while)")
+    else: 
+    	# only apply the model to a limited number of items
+    	shuffle(unlabeled_data)
+    	unlabeled_data = unlabeled_data[:limit]
+    
     with torch.no_grad():
-        c=0
         for item in unlabeled_data:
             textid = item[0]
             if textid in already_labeled:
@@ -293,7 +319,9 @@ def get_low_conf_unlabeled(model, unlabeled_data, number=80):
             feature_vector = make_feature_vector(text.split(), feature_index)
             log_probs = model(feature_vector)
 
-            prob_related = math.exp(log_probs.data.tolist()[0][1]) # get confidence that it is related
+            # get confidence that it is related
+            prob_related = math.exp(log_probs.data.tolist()[0][1]) 
+            
             if prob_related < 0.5:
                 confidence = 1 - prob_related
             else:
@@ -302,9 +330,6 @@ def get_low_conf_unlabeled(model, unlabeled_data, number=80):
             item[3] = "low confidence"
             item[4] = confidence
             confidences.append(item)
-            c+=1
-            if c > 10000:
-                break
 
     confidences.sort(key=lambda x: x[4])
     return confidences[:number:]
@@ -323,24 +348,26 @@ def get_random_items(unlabeled_data, number = 10):
             break
 
     return random_items
-        
+
 
 def get_outliers(training_data, unlabeled_data, number=10):
     """Get outliers from unlabeled data in training data
-    return number outliers
+
+    Returns number outliers
+    
+    An outlier is defined as the percent of words in an item in 
+    unlabeled_data that do not exist in training_data
     """
     outliers = []
 
-    total_feature_counts = {}
+    total_feature_counts = defaultdict(lambda: 0)
+    
     for item in training_data:
         text = item[1]
         features = text.split()
 
         for feature in features:
-            if feature in total_feature_counts:
-                total_feature_counts[feature] += 1
-            else:
-                total_feature_counts[feature] = 1
+            total_feature_counts[feature] += 1
                 
     while(len(outliers) < number):
         top_outlier = []
@@ -353,7 +380,7 @@ def get_outliers(training_data, unlabeled_data, number=10):
 
             text = item[1]
             features = text.split()
-            total_matches = 1 # start at 1 for slight smoothing to avoid only short sentences
+            total_matches = 1 # start at 1 for slight smoothing 
             for feature in features:
                 if feature in total_feature_counts:
                     total_matches += total_feature_counts[feature]
@@ -363,23 +390,21 @@ def get_outliers(training_data, unlabeled_data, number=10):
                 top_match = ave_matches
                 top_outlier = item
 
-        # add this outlier to list and update what is 'labeled', assuming this new outlier will get a label
+        # add this outlier to list and update what is 'labeled', 
+        # assuming this new outlier will get a label
         top_outlier[3] = "outlier"
         outliers.append(top_outlier)
         text = top_outlier[1]
         features = text.split()
         for feature in features:
-            if feature in total_feature_counts:
-                total_feature_counts[feature] += 1
-            else:
-                total_feature_counts[feature] = 1
+            total_feature_counts[feature] += 1
 
     return outliers
     
 
 
 def evaluate_model(model, evaluation_data):
-    """Evalute the model on the held-out evaluation data
+    """Evaluate the model on the held-out evaluation data
 
     Return the f-value for disaster-related and the AUC
     """
@@ -387,9 +412,9 @@ def evaluate_model(model, evaluation_data):
     related_confs = [] # related items and their confidence of being related
     not_related_confs = [] # not related items and their confidence of being _related_
 
-    tp = 0.0 # true positives, etc 
-    fp = 0.0
-    fn = 0.0
+    true_pos = 0.0 # true positives, etc 
+    false_pos = 0.0
+    false_neg = 0.0
 
     with torch.no_grad():
         for item in evaluation_data:
@@ -398,32 +423,33 @@ def evaluate_model(model, evaluation_data):
             feature_vector = make_feature_vector(text.split(), feature_index)
             log_probs = model(feature_vector)
 
-            prob_related = math.exp(log_probs.data.tolist()[0][1]) # get confidence that it is related
+            # get confidence that item is disaster-related
+            prob_related = math.exp(log_probs.data.tolist()[0][1]) 
 
             if(label == "1"):
                 # true label is disaster related
                 related_confs.append(prob_related)
                 if prob_related > 0.5:
-                    tp+=1.0
+                    true_pos += 1.0
                 else:
-                    fn += 1
+                    false_neg += 1.0
             else:
                 # not disaster-related
                 not_related_confs.append(prob_related)
                 if prob_related > 0.5:
-                    fn+=1.0
+                    false_pos += 1.0
 
     # Get FScore
-    if tp == 0.0:
+    if true_pos == 0.0:
         fscore = 0.0
     else:
-        precision = tp / (tp + fp)
-        recall = tp / (tp + fn)
+        precision = true_pos / (true_pos + false_pos)
+        recall = true_pos / (true_pos + false_neg)
         fscore = (2 * precision * recall) / (precision + recall)
 
     # GET AUC
     not_related_confs.sort()
-    total_greater = 0 # count of how many total are greater
+    total_greater = 0 # count of how many total have higher confidence
     for conf in related_confs:
         for conf2 in not_related_confs:
             if conf < conf2:
@@ -443,7 +469,13 @@ if evaluation_count <  minimum_evaluation_items:
     #Keep adding to evaluation data first
     print("Creating evaluation data:\n")
 
+    shuffle(data)
+    needed = minimum_evaluation_items - evaluation_count
+    data = data[:needed]
+    print(str(needed)+" more annotations needed")
+
     data = get_annotations(data) 
+	
     related = []
     not_related = []
 
@@ -462,7 +494,13 @@ elif training_count < minimum_training_items:
     # lets create our first training data! 
     print("Creating initial training data:\n")
 
+    shuffle(data)
+    needed = minimum_training_items - training_count
+    data = data[:needed]
+    print(str(needed)+" more annotations needed")
+
     data = get_annotations(data)
+
     related = []
     not_related = []
 
@@ -478,17 +516,17 @@ elif training_count < minimum_training_items:
     append_data(training_not_related_data, not_related)
 else:
     # lets start Active Learning!! 
+
+	# Train new model with current training data
+    vocab_size = create_features()
+    model_path = train_model(training_data, evaluation_data=evaluation_data, vocab_size=vocab_size)
+
     print("Sampling via Active Learning:\n")
 
-    # load most recent model
-    models = os.listdir('./models')
-    models.sort()
-    model_path = "models/"+ models[-1]
-
-    vocab_size = create_features()
     model = SimpleTextClassifier(2, vocab_size)
     model.load_state_dict(torch.load(model_path))
 
+	# get 100 items per iteration with the following breakdown of strategies:
     random_items = get_random_items(data, number=10)
     low_confidences = get_low_conf_unlabeled(model, data, number=80)
     outliers = get_outliers(training_data+random_items+low_confidences, data, number=10)
@@ -509,17 +547,17 @@ else:
     # append training data
     append_data(training_related_data, related)
     append_data(training_not_related_data, not_related)
-
-
-# UPDATE OUR DATA AND (RE)TRAIN MODEL WITH NEWLY ANNOTATED DATA
-training_data = load_data(training_related_data) + load_data(training_not_related_data)
-training_count = len(training_data)
-
-evaluation_data = load_data(evaluation_related_data) + load_data(evaluation_not_related_data)
-evaluation_count = len(evaluation_data)
+    
 
 if training_count > minimum_training_items:
     print("\nRetraining model with new data")
+    
+	# UPDATE OUR DATA AND (RE)TRAIN MODEL WITH NEWLY ANNOTATED DATA
+    training_data = load_data(training_related_data) + load_data(training_not_related_data)
+    training_count = len(training_data)
+
+    evaluation_data = load_data(evaluation_related_data) + load_data(evaluation_not_related_data)
+    evaluation_count = len(evaluation_data)
 
     vocab_size = create_features()
     model_path = train_model(training_data, evaluation_data=evaluation_data, vocab_size=vocab_size)
