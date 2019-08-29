c916d5ddcc594eca68571c243705d14c2b272d91
Robert Munro <robert.munro@gmail.com>
2019-08-29 00:12:06 -0700

    cleaned functions and ordered as in the text

--- uncertainty_sampling_pytorch.py
+++ uncertainty_sampling_pytorch.py
@@ -2,25 +2,28 @@ import torch
 import math
 
 
-def softmax(tensor, base=math.exp(1)):
+def least_confidence(prob_dist, sorted=False):
 	""" 
-	Converts a set of raw scores from a model into a 
-	probability distribution via softmax.
-		
-	The probability distribution will be a set of real numbers
-	such that each is in the 0-1 range and the sum is 1.0
-
-	Assumes input is a pytorch tensor in the form: tensor([1.0, 4.0, 2.0, 3.0])
-		
-	Keyword arguments:
-		tensor -- a pytorch tensor of any positive/negative real numbers.
-		base -- the base for the exponential (default e)
-	"""
-	exps = base**tensor # exponential for each value in array
+	Returns the uncertainty score of an array using
+	least confidence sampling in a 0-1 range where 1 is the most uncertain
 	
-	sum_exps = torch.sum(exps) # sum of all exponentials
-	prob_dist = exps / sum_exps # normalize exponentials 
-	return prob_dist
+	Assumes probability distribution is a pytorch tensor, like: 
+		tensor([0.0321, 0.6439, 0.0871, 0.2369])
+				
+	Keyword arguments:
+		prob_dist -- a pytorch tensor of real numbers between 0 and 1 that total to 1.0
+		sorted -- if the probability distribution is pre-sorted from largest to smallest
+	"""
+	if sorted:
+		simple_least_conf = prob_dist.data[0] # most confident prediction
+	else:
+		simple_least_conf = torch.max(prob_dist) # most confident prediction
+				
+	num_labels = prob_dist.size(0) # number of labels
+	
+	normalized_least_conf = (1 - simple_least_conf) * (num_labels / (num_labels -1))
+	
+	return normalized_least_conf.item()
 
 
 def margin_confidence(prob_dist, sorted=False):
@@ -62,33 +65,6 @@ def ratio_confidence(prob_dist, sorted=False):
 	ratio_conf = prob_dist.data[1] / prob_dist.data[0]
 	
 	return ratio_conf.item()
-	
-	
-
-
-def least_confidence(prob_dist, sorted=False):
-	""" 
-	Returns the uncertainty score of an array using
-	least confidence sampling in a 0-1 range where 1 is the most uncertain
-	
-	Assumes probability distribution is a pytorch tensor, like: 
-		tensor([0.0321, 0.6439, 0.0871, 0.2369])
-				
-	Keyword arguments:
-		prob_dist -- a pytorch tensor of real numbers between 0 and 1 that total to 1.0
-		sorted -- if the probability distribution is pre-sorted from largest to smallest
-	"""
-	if sorted:
-		simple_least_conf = prob_dist.data[0] # most confident prediction
-	else:
-		simple_least_conf = torch.max(prob_dist) # most confident prediction
-				
-	num_labels = prob_dist.size(0) # number of labels
-	
-	normalized_least_conf = (1 - simple_least_conf) * (num_labels / (num_labels -1))
-	
-	return normalized_least_conf.item()
-
 
 
 def entropy_score(prob_dist):
@@ -111,4 +87,4 @@ def entropy_score(prob_dist):
 	return normalized_entropy.item()
 	
 	
-
+	
